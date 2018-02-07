146d8c533bdd69e48131af9192fd410c8ee0bb3a
radigued <radigue.damien@orange.fr>
2018-02-07 13:48:45 +0100

    Updated

--- services/API_service.js
+++ services/API_service.js
@@ -1,4 +1,4 @@
-< script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" > < /script> 
+< script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" > < /script>
 < script src = "http://ajax.googleapis.com/ajax/libs/angularjs/1.0.4/angular.js" > < /script>
 
 //Service d'appel à l'api permettant de gerer la visionneuse
@@ -19,12 +19,12 @@ angular.module('LISA').service('AppelWsSvc', function ($http) {
 
     this.testConnection = function (login, password) {
         var url = this.prefixUrl + "users?login=" + login + "&password=" + password;
-        
+
         return $http.get(url, this.config);
     };
-    
-    
-    
+
+
+
     this.getProducts = function () {
         var url = this.prefixUrl + "products";
         var tab[];
@@ -33,11 +33,25 @@ angular.module('LISA').service('AppelWsSvc', function ($http) {
         });
         return tab;
     }
-    
-    
-    
+
+    var productsElt = document.getElementById("products");
+    ajaxGet(this.prefixUrl + "products", function (reponse) {
+        // Transforme la réponse en un tableau d'articles
+        var products = JSON.parse(reponse);
+        products.forEach(function (product) {
+            // Ajout du titre et du contenu de chaque article
+            var titreElt = document.createElement("h3");
+            titreElt.textContent = product.label;
+            var contenuElt = document.createElement("p");
+            contenuElt.textContent = product.description;
+            productsElt.appendChild(titreElt);
+            productsElt.appendChild(contenuElt);
+        });
+    });
+
+
     this.getProductsList = function ();
-    
+
     this.getProductsLabel = function (item) {
         var url = this.prefixUrl + "products";
         return item.Label;
--- views/index.php
+++ views/index.php
@@ -3,11 +3,10 @@
     
     <!-- Header and links -->
     <?php include("../views/Header.php") ?>
-    <?php include("../SQLConnection.php") ?>
     <script type="text/javascript" src="../API_connection.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.0.4/angular.js"></script>
-    <body ng-app="LISA" ng-controller="LISA_COntroller">
+    <body ng-app="LISA" ng-controller="LISA_COntroller" ng-module="LISA">
         <!-- Barre de menu + recherche -->
         <?php include("./Menu.php") ?>
     
@@ -15,7 +14,7 @@
             <select class="col-xs-12">
                 <option>-- Choisissez un article --</option>
                 <script src="../API_connection.js"> 
-                    getProducts();               
+
                 </script>
             </select>
         </div>
